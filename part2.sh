b2a32137d93559d09c0fe22d1c12bb99be6bd751
Michelle Corley <mcorley1@nd.edu>
2017-09-19 13:23:35 -0400

    updated and correct version of code for part 2 of challenge

--- part2.sh
+++ part2.sh
@@ -2,4 +2,9 @@
 # Also tells the number of females in the top 10 earners in the data sel
 # Usage: bash part2.sh wages.csv
 
-cat wages.csv | sort -n -k 4 | tail -n 1 | tr -s " " | cut -d " " -f 1,2
+echo "information for lowest wage earner" 
+cat wages.csv | grep -v "gender" | tr "," " " | sort -n -k 4 | head -n 1 | cut -d " " -f 1,2,4
+echo "information for highest wage earner" 
+cat wages.csv | grep -v "gender" | tr "," " " | sort -n -k 4 | tail -n 1 | cut -d " " -f 1,2,4
+echo "number of females in the top ten earners" 
+cat wages.csv | tr "," " " | sort -n -k 4 | tail -n 10 | grep "female" | wc -l
