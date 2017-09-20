06c90aa6fb6f8ff40fb49e7aada89404e2b62831
Mati Nemera <mnemera@nd.edu>
2017-09-20 13:52:59 -0400

    Minimum wage 12 vs 16 with variables 1 and 2

--- Exercise4.sh
+++ Exercise4.sh
@@ -13,3 +13,7 @@ cat wages.csv | sed 's/,/ /g' | cut -d ' ' -f 1,2,4 | sort -n -k 3 | head -2 | t
 #Number of females in top 10 earners
 echo "Number of Females in Top 10 Earners"
 cat wages.csv | sed 's/,/ /g' | sort -n -k 4 | tail -10 | grep "female" | wc -l
+
+#Minimum wages of 12 vs 16 years school
+$ cat wages.csv | awk -F, '$3 = "12"' | sort -n -k "4" | head -2 | tail -1
+
