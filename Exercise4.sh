fd21ce2a24b5d954e3bc2df58c5d7d457143a5ac
Katherine <bookaholic214@gmail.com>
2017-09-20 13:13:04 -0400

    Part 3 Question 2 added

--- Exercise4.sh
+++ Exercise4.sh
@@ -4,9 +4,12 @@
 #Q2
 #Print gender, yearsExperience, and wage for highest earner
 echo "Highest Earner"
-cat wages.csv | sed 's/,/ /g' | cut -d ' ' -f 1,2,4 | sort -n -k 3 | head -2 | tail 
--1
+cat wages.csv | sed 's/,/ /g' | cut -d ' ' -f 1,2,4 | sort -n -k 3 | tail -1 
+
 #Print gender, yearsExperience, and wage for lowest earner
 echo "Lowest Earner"
-cat wages.csv | sed 's/,/ /g' | cut -d ' ' -f 1,2,4 | sort -n -k 3 | tail -1
+cat wages.csv | sed 's/,/ /g' | cut -d ' ' -f 1,2,4 | sort -n -k 3 | head -2 | tail -1
 
+#Number of females in top 10 earners
+echo "Number of Females in Top 10 Earners"
+cat wages.csv | sed 's/,/ /g' | sort -n -k 4 | tail -10 | grep "female" | wc -l
