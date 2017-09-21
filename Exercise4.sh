a2e222730e3db312fbd85c915dfbb7ab187e00f4
omegadan01 <dbruzzes@nd.edu>
2017-09-21 09:57:04 -0400

    added line to remove tmp file in q1

--- Exercise4.sh
+++ Exercise4.sh
@@ -5,6 +5,7 @@
 cat wages.csv | cut -d ',' -f1,2 | sed 's/\<[0-9]\>/0&/' | sed 's/,/ /g' > a.tmp.csv
 head -1 a.tmp.csv > u.gender.year.csv
 sed '1d' a.tmp.csv |  sort -n | uniq >>u.gender.year.csv
+rm a.tmp.csv
 echo "unique gender and experience .csv complete"
 
 #Q2
