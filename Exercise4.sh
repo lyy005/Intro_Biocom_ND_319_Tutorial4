9b6923f6d7d49f0cf1065c7f594e92c6a3dd150e
pjlmac <pjlmac@users.noreply.github.com>
2017-09-21 14:12:50 -0400

    Delete Exercise4.sh

--- Exercise4.sh
+++
@@ -1,3 +0,0 @@
-grep -w "female" wages.csv | cut -d , -f1,2 | tr ',' ' ' | sort -n -k2 | uniq >> Wages1.txt
-grep -w "male" wages.csv | cut -d , -f1,2 | tr ',' ' ' | sort -n -k2 | uniq >> Wages1.txt
-
