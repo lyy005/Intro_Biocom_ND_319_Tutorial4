a886e13f5602d7939fd3ab5efe6c92c7ac3774cb
Balaji Sampathkumar <bsampath@nd.edu>
2017-09-21 15:33:04 -0400

    Modified with #1 and stdout on 2 and 3

--- earnings.sh
+++ earnings.sh
@@ -1,27 +1,32 @@
 #Script prints the following information for a given dataset:
+#Unique gender-yearsExperience combinations
 #Gender, years of Experience, wage for highest earner
 #Gender, years of experience, wage for lowest earner
 #Number of women in top 10 earners
 #Effect of graduating college on the minumum wage of earners
 
 #Usage: bash earnings.sh filename.csv
+#1:
+cat $1 | sed 's/,/ /g' | cut -d ' ' -f 1,2 | uniq | sort -n -k 1 | sort -n -k 2 | uniq > output_ex4.txt
 
-echo "Highest Earner:"
+#2:
+echo "Highest Earner:" >> output_ex4.txt
 
-cat $1 | sed s/","/" "/g | sort -n -k4 | tail -n 1 | cut -d ' ' -f 1,2,4
+cat $1 | sed s/","/" "/g | sort -n -k4 | tail -n 1 | cut -d ' ' -f 1,2,4 >> output_ex4.txt
 
-echo "Lowest Earner:"
+echo "Lowest Earner:" >> output_ex4.txt
 
-cat $1 | sed s/","/" "/g | sort -n -k4 | head -n 2 | tail -n 1 | cut -d ' ' -f 1,2,4
+cat $1 | sed s/","/" "/g | sort -n -k4 | head -n 2 | tail -n 1 | cut -d ' ' -f 1,2,4 >> output_ex4.txt
 
-echo "Women in Top 10 Earners:"
+echo "Women in Top 10 Earners:" >> output_ex4.txt
 
-cat $1 | sed s/","/" "/g | sort -n -k4 | tail -n 10 | grep -c "female" 
+cat $1 | sed s/","/" "/g | sort -n -k4 | tail -n 10 | grep -c "female" >> output_ex4.txt
 
 val1=$(cat $1 | sed s/","/" "/g | awk '$3=="12"' | sort -n -k4 | head -n 1 | cut -d ' ' -f 4)
 
 val2=$(cat $1 | sed s/","/" "/g | awk '$3=="16"' | sort -n -k4 | head -n 1 | cut -d ' ' -f 4)
 
-echo "Effect of Graduating College on Minimum Wage:"
+#3:
+echo "Effect of Graduating College on Minimum Wage:" >> output_ex4.txt
 
-echo "$val2-$val1" | bc
+echo "$val2-$val1" | bc >> output_ex4.txt
