7f356d0d1b2953275416f9f295be3db6cfb7c972
kkilgoreND <31992787+kkilgoreND@users.noreply.github.com>
2017-09-21 12:36:38 -0400

    code for part 1

--- part1script.sh
+++ part1script.sh
@@ -3,9 +3,7 @@
 
 # copies data from wages.csv to new file called part1challenge.csv
 cp wages.csv part1challenge.csv
-
 #remove 1st line w/ column titles(allows for no confusion in further commands)
-sed -i '1d' wages.csv
-
+sed -i '1d' part1challenge.csv
 #remove columns other than gender and yearsExperience; sort 1st by gender then numerically by yearsExperience(keeping pairs together); replace comma delemiter with space
-awk '{print $1,$2}' $filename | sort -t, -k1,1 -nk2,2 | cut -d, -f1,2 | tr ',' ' '
+awk '{print $1,$2}' part1challenge.csv | sort -t, -k1,1 -nk2,2 | cut -d, -f1,2 | tr ',' ' '
