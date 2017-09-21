a62e23be28276c923bb4e044984c4e419d05933b
Phil McCown <pjlmac@gmail.com>
2017-09-20 23:26:20 -0400

    Fixed Repeat Problem, now we need to subtract output

--- Exercise4C.sh
+++ Exercise4C.sh
@@ -1,6 +1,6 @@
-for wage in $1, $2
+for wage in $@
 do
 #for input, go with the higher numbered term and only using the format gender,yearsOfschooling, for example, "male,16" or "female,16"
-cut -d , -f1,3,4 wages.csv | grep -we "$1" | cut -d , -f3 | sort -rn | tail -n 1 
-cut -d , -f1,3,4 wages.csv | grep -we "$2" | cut -d , -f3 | sort -rn | tail -n 1
+cut -d , -f1,3,4 wages.csv | grep -we $wage | cut -d , -f3 | sort -rn | 
+tail -n 1 
 done
