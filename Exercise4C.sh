16e7d7cfe7512e99665d39677da2be5fdf6821aa
Phil McCown <pjlmac@gmail.com>
2017-09-21 00:17:04 -0400

    No change

--- Exercise4C.sh
+++ Exercise4C.sh
@@ -1,6 +1,6 @@
 for wage in $@
 do
-#for input, go with the higher numbered term and only using the format gender,yearsOfschooling, for example, "male,16" or "female,16"
-cut -d , -f1,3,4 wages.csv | grep -we $wage | cut -d , -f3 | sort -rn | 
-tail -n 1 
+#for input, go with the higher numbered term first and only using the format gender,yearsOfschooling, for example, "male,16" or "female,16"
+cut -d , -f1,3,4 wages.csv | grep -we $wage | cut -d , -f3 | sort -rn | tail -n 1
 done
+
